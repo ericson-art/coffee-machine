df3ecb32be8fd6f36b5cc863d0d68ed1b259f80c
ERNEST ARTIUHOV <es.artxxx@gmail.com>
2022-02-24 14:15:52 +0300

    commit_5

--- src/main/java/er/art/CoffeDrinks.java
+++ src/main/java/er/art/CoffeDrinks.java
@@ -1,1 +1,1 @@
-package er.art;public interface CoffeDrinks {    public void calkOfIngredients ();    void setPrintMSG (int waterInMachine, int coffeeInMachine, int milkInMachine);    public Boolean isCalculate ();    public String getNameDrink ();    public StringBuilder getPrintMSG ();}
+package er.art;public interface CoffeDrinks {    public String getNameDrink ();    public StringBuilder getPrintMSG ();    void setPrintMSG (int waterInMachine, int coffeeInMachine, int milkInMachine);    public void calkOfIngredients ();    public Boolean isCalculate ();}
--- src/main/java/er/art/CoffeeMachine.java
+++ src/main/java/er/art/CoffeeMachine.java
@@ -17,41 +17,6 @@ public class CoffeeMachine {
         this.milk = Integer.parseInt ( milk );
     }
 
-    public String getStartMsg () {
-        String msg = "\nПример команд:\n" +
-                     " • water N - залить N мл воды." +
-                     "\n" +
-                     " • milk N - залить N мл молока." +
-                     "\n" +
-                     " • coffee N - засыпать N мл кофе." +
-                     "\n" +
-                     " • latte N - приготовить латте на N мл. Приготовление происходит только при N ≥ 10." +
-                     "\n" +
-                     " • ristretto N - приготовить ристретто на N мл. Приготовление происходит только при N ≥ 10." +
-                     "\n" +
-                     " • cappuccino N - приготовить капучино на N мл. Приготовление происходит только при N ≥ 10." +
-                     "\n" +
-                     " • lungo N - приготовить лунго на N мл. Приготовление происходит только при N ≥ 10." +
-                     "\n" +
-                     " • espresso N - приготовить эспрессо на N мл. Приготовление происходит только при N ≥ 10." +
-                     "\n\n" +
-                     " • turn off - отключение. После получения команды отключения кофемашина должна завершить свою работу.";
-        return msg.trim ();
-    }
-
-    public void printValueAdd (String msg) {
-        String out = String.format ( "%s %s %s  ---> ( Добавили %s )", coffee, water, milk, msg );
-        System.out.println ( out );
-    }
-
-    public String getMsgValueAddError () {
-        return String.format ( "В машине:\n" +
-                               " • %d мл. кофе\n" +
-                               " • %d мл. воды\n" +
-                               " • %d мл. молока\n\n", coffee, water, milk ).trim ();
-    }
-
-
     public int getCoffee () {
         return coffee;
     }
@@ -84,15 +49,49 @@ public class CoffeeMachine {
         return functionList;
     }
 
+    public void printValueAdd (String msg) {
+        String out = String.format ( "%s %s %s  ---> ( Добавили %s )", coffee, water, milk, msg );
+        System.out.println ( out );
+    }
+
+    public String getMsgValueAddError () {
+        return String.format ( "В машине:\n" +
+                               " • %d мл. кофе\n" +
+                               " • %d мл. воды\n" +
+                               " • %d мл. молока\n\n", coffee, water, milk ).trim ();
+    }
+
     private String resultString (CoffeDrinks drink) {
         if (drink.isCalculate ())
-            return drink.getPrintMSG ().append (String.format ( "    -->    Ваш %s готов!",drink.getNameDrink () ) ).toString ();
+            return drink.getPrintMSG ().append ( String.format ( "    -->    Ваш %s готов!", drink.getNameDrink () ) ).toString ();
 
         else
             return drink.getPrintMSG ().
                     append ( "\n" ).
                     append ( this.getMsgValueAddError () ).
                     append ( "\n" ).toString ();
+    }
+
+    public String getStartMsg () {
+        String msg = "\nПример команд:\n" +
+                     " • water N - залить N мл воды." +
+                     "\n" +
+                     " • milk N - залить N мл молока." +
+                     "\n" +
+                     " • coffee N - засыпать N мл кофе." +
+                     "\n" +
+                     " • latte N - приготовить латте на N мл. Приготовление происходит только при N ≥ 10." +
+                     "\n" +
+                     " • ristretto N - приготовить ристретто на N мл. Приготовление происходит только при N ≥ 10." +
+                     "\n" +
+                     " • cappuccino N - приготовить капучино на N мл. Приготовление происходит только при N ≥ 10." +
+                     "\n" +
+                     " • lungo N - приготовить лунго на N мл. Приготовление происходит только при N ≥ 10." +
+                     "\n" +
+                     " • espresso N - приготовить эспрессо на N мл. Приготовление происходит только при N ≥ 10." +
+                     "\n\n" +
+                     " • turn off - отключение. После получения команды отключения кофемашина должна завершить свою работу.";
+        return msg.trim ();
     }
 
     public void makeDrink (CoffeDrinks drink) {
